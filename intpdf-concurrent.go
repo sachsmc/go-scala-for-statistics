4b01af99a8b646afbc8e0d05ac6b4d6001c93e0b
Michael Sachs <sachsmc@gmail.com>
2015-04-13 15:49:50 -0400

    Go code

---
+++ intpdf-concurrent.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+	"runtime"
+)
+
+func Dnorm(x float64) float64 {
+
+	return math.Exp(-x*x/2) / math.Sqrt(2*math.Pi)
+
+}
+
+func main() {
+
+  runtime.GOMAXPROCS(runtime.NumCPU())
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	N := 0
+	if len(os.Args) == 1 {
+
+		N = 5000
+
+	} else {
+
+		Na, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+
+		N = Na
+
+	}
+
+	res := float64(0.0)
+	for i := 0; i < N; i++ {
+
+		go func(i int) {
+			candx := rand.Float64()*10 - 5
+			candy := rand.Float64() * .5
+
+			if candy <= Dnorm(candx) {
+				res = res + 1
+			}
+		}(i)
+	}
+
+	// the rectangle is 10 by 0.5 = 5.0
+
+	fmt.Fprintf(os.Stdout, "%f8\n", 5.0*res/float64(N))
+
+}
